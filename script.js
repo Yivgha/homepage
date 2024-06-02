00068d3a2482dd54aa723f67b4844e386e0903e9
Yivgha <genieukne@gmail.com>
2024-06-02 20:18:28 +0300

    added favicon and funny sounds

--- script.js
+++ script.js
@@ -30,3 +30,67 @@ spans.forEach((span, index) => {
   span.style.animationDelay = `${index * delay}ms`;
 });
 
+// AUDIO
+const playBookSound = document.getElementById('play-book');
+const clickBookSound = document.getElementById('click-book');
+const playGameSound = document.getElementById('play-game');
+const clickGameSound = document.getElementById('click-game');
+const clickElectroSound = document.getElementById('click-electro');
+const playElectroSound = document.getElementById('play-electro');
+const clickPianoSound = document.getElementById('click-piano');
+const playPianoSound = document.getElementById('play-piano');
+
+playBookSound.addEventListener('click', (e) => {
+  e.preventDefault();
+  pauseAllAudio();
+  clickBookSound.play();
+});
+
+playGameSound.addEventListener('click', (e) => {
+  e.preventDefault();
+  pauseAllAudio();
+  clickGameSound.play();
+});
+
+playElectroSound.addEventListener('click', (e) => {
+  e.preventDefault();
+  pauseAllAudio();
+  clickElectroSound.play();
+});
+
+playPianoSound.addEventListener('click', (e) => {
+  e.preventDefault();
+  pauseAllAudio();
+  clickPianoSound.play();
+});
+
+function pauseAllAudio() {
+  clickBookSound.pause();
+  clickGameSound.pause();
+  clickElectroSound.pause();
+  clickPianoSound.pause();
+}
+
+document.body.addEventListener('click', (e) => {
+  const target = e.target;
+  if (
+    !isDescendantOrSelf(target, playBookSound) &&
+    !isDescendantOrSelf(target, playGameSound) &&
+    !isDescendantOrSelf(target, playElectroSound) &&
+    !isDescendantOrSelf(target, playPianoSound)
+  ) {
+    pauseAllAudio();
+  }
+});
+
+function isDescendantOrSelf(element, ancestor) {
+  let node = element;
+  while (node) {
+    if (node === ancestor) {
+      return true;
+    }
+    node = node.parentElement;
+  }
+  return false;
+}
+
